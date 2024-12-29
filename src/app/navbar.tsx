23019534d18959970a11504c47d0d91bfcecc948
Jake Spievak <jakespvk@gmail.com>
2024-12-29 00:18:24 -0800

    add eyes

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Image from "next/image"
+import eyeImg from "../../eye.png";
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -16,28 +18,18 @@ import {
     Drawer,
     DrawerClose,
     DrawerContent,
-    DrawerDescription,
+    //DrawerDescription,
     DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer"
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
     const [isScrolled, setIsScrolled] = useState(false);
-
-    if (typeof window !== 'undefined') {
-        let navElement = document.getElementById("main-nav");
-        if (navElement !== null) {
-            if (isScrolled) {
-                navElement.classList.add("border-b border-gray-300");
-            } else {
-                navElement.classList.remove("border-b border-gray-300");
-            }
-        }
-    }
+    const eyeRef = useRef<HTMLImageElement | null>(null);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -50,11 +42,28 @@ export default function Navbar() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    function toggleEye() {
+        if (eyeRef !== null) {
+            let passwordInput = document.getElementById("password");
+            let eyeButton = document.getElementById("eyeButton");
+            if (passwordInput !== null && eyeButton !== null) {
+                if (passwordInput.getAttribute("type") === "password") {
+                    passwordInput.setAttribute("type", "text");
+                    eyeButton.classList.remove("fa-eye");
+                    eyeButton.classList.add("fa-eye-slash");
+                } else {
+                    passwordInput.setAttribute("type", "password");
+                    eyeButton.classList.add("fa-eye");
+                    eyeButton.classList.remove("fa-eye-slash");
+                }
+            }
+        }
+    }
+
     return (
 
-        <nav className={`fixed top-0 left-0 w-full bg-black transition-[box-shadow] duration-700 ease-out ${isScrolled ? 'shadow-lg shadow-gray-900/70' : 'ease-in duration-300'
-            }`}>
-            <div className="max-w-[96rem] mx-auto px-4 sm:px-6 lg:px-8">
+        <nav className={`fixed top-0 left-0 w-full bg-black transition-[box-shadow] duration-700 ease-out ${isScrolled ? 'shadow-lg shadow-gray-900/70' : 'ease-in duration-300'}`}>
+            <div className="max-w-[96rem] mx-auto px-8 py-2">
                 <div className="flex items-center justify-between h-16">
                     {/*<!-- Logo (Left) -->*/}
                     <div className="flex-1">
@@ -71,8 +80,8 @@ export default function Navbar() {
                                 </DrawerHeader>
                                 <DrawerFooter className="text-xl p-4 mx-5">
                                     <a href="/about">About</a>
-                                    <a>Pricing</a>
-                                    <a>Your Data</a>
+                                    <a href="/pricing">Pricing</a>
+                                    <a href="/data">Your Data</a>
                                     <DrawerClose className="pt-5">
                                         <div className="pt-5 pb-5 text-base space-x-3 flex justify-center">
                                             <a className="border text-center bg-gray-300 text-gray-950 w-24 px-2 py-1 rounded-md">Sign In</a>
@@ -105,23 +114,28 @@ export default function Navbar() {
                                         Make changes to your profile here. Click save when you're done.
                                     </DialogDescription>
                                 </DialogHeader>
-                                <div className="grid gap-4 py-4">
-                                    <div className="grid grid-cols-4 items-center gap-4">
-                                        <Label htmlFor="name" className="text-right">
-                                            Name
-                                        </Label>
-                                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                                <form action="" method="post">
+                                    <div className="grid gap-4 py-4">
+                                        <div className="grid grid-cols-4 items-center gap-4">
+                                            <Label htmlFor="email" className="text-right">
+                                                Email
+                                            </Label>
+                                            <Input id="email" placeholder="john@appleseeds.com" className="text-gray-950 col-span-3" />
+                                        </div>
+                                        <div className="grid grid-cols-4 items-center gap-4">
+                                            <Label htmlFor="password" className="text-right">
+                                                Password
+                                            </Label>
+                                            <Input id="password" type="password" placeholder="Password..." className="text-gray-950 col-span-3" />
+                                            <span onClick={toggleEye} className="z-[100] translate-x-[21.5rem] -translate-y-11">
+                                                <Image alt="eyes" src={eyeImg} width={18} height={18} id="eyeButton" ref={eyeRef} />
+                                            </span>
+                                        </div>
                                     </div>
-                                    <div className="grid grid-cols-4 items-center gap-4">
-                                        <Label htmlFor="username" className="text-right">
-                                            Username
-                                        </Label>
-                                        <Input id="username" value="@peduarte" className="col-span-3" />
-                                    </div>
-                                </div>
-                                <DialogFooter>
-                                    <Button type="submit">Save changes</Button>
-                                </DialogFooter>
+                                    <DialogFooter>
+                                        <Button type="submit">Sign In</Button>
+                                    </DialogFooter>
+                                </form>
                             </DialogContent>
                         </Dialog>
                         <Dialog>
@@ -147,6 +161,9 @@ export default function Navbar() {
                                             Username
                                         </Label>
                                         <Input id="username" value="@peduarte" className="col-span-3" />
+                                        <span onClick={toggleEye} className="">
+                                            <i id="eyeButton" ref={eyeRef} className="fa-solid fa-eye text-lg text-gray-300"></i>
+                                        </span>
                                     </div>
                                 </div>
                                 <DialogFooter>
