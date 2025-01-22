da529bfb9a8da9889ca09a8d0cdd665c50657466
Jake Spievak <jakespvk@gmail.com>
2025-01-21 21:23:17 -0800

    dashboard in progress

---
+++ src/app/dashboard/DatabaseForm.tsx
@@ -0,0 +1,8 @@
+export default function DatabaseForm({ provider }: { provider: string }) {
+	return (
+		<div>
+			<h2>Database Form</h2>
+			<p>{provider}</p>
+		</div>
+	)
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,48 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
+import { Button } from "@/components/ui/button";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react";
+import DatabaseForm from "./DatabaseForm";
 
 export default function Dashboard() {
 	const { logout } = useAuth();
+	const [selectedProvider, setSelectedProvider] = useState('');
+
+	console.log(selectedProvider);
+
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1>Dashboard</h1>
-			<button onClick={logout}>Logout</button>
-		</div>
+		<div>
+			<div className="w-full h-svh flex justify-center items-center">
+				<div className="glass-card">
+					<div className="glass-card-contents">
+						<h2 className="text-center text-xl mb-3">Dashboard</h2>
+						<Select value={selectedProvider} onValueChange={setSelectedProvider}>
+							<SelectTrigger className="w-[180px]">
+								<SelectValue placeholder="Select a database provider" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value="SQLite">SQLite</SelectItem>
+								<SelectItem value="ActiveCampaign">ActiveCampaign</SelectItem>
+								<SelectItem value="Other">Other</SelectItem>
+							</SelectContent>
+						</Select>
+
+						{selectedProvider === 'SQLite' ? <DatabaseForm provider="SQLite" /> : (selectedProvider === 'ActiveCampaign' ? <DatabaseForm provider="ActiveCampaign" /> : <></>)}
+
+						<div className="flex items-center justify-center">
+							<Button className="mt-7" variant="outline" onClick={logout}>Logout</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div >
 	)
 }
