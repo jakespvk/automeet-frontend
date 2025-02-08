8b021a985e72eca68802d1b31ec0d01772c8055d
Jake Spievak <jakespvk@gmail.com>
2025-02-08 14:16:46 -0800

    fix rewrites???

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	env: {
+		NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+	},
 	async rewrites() {
 		return [
 			{
 				source: '/:path*',
 				destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+				permanent: true,
 			},
 		];
 	},
