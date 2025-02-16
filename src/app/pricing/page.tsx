0dfe32ad5d537a0385a6ab26832142838cb0a806
Jake Spievak <jakespvk@gmail.com>
2025-02-15 21:46:00 -0800

    update pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -6,13 +6,37 @@ import { useState } from "react";
 const Pricing = (props: {}) => {
 	const [columns, setColumns] = useState([5]);
 	const [rows, setRows] = useState([50]);
-	const PRICE = calculatePrice(columns[0], rows[0]);
+	const [pollFrequency, setPollFrequency] = useState([1]);
+	const PRICE = calculatePrice(columns[0], rows[0], pollFrequency[0]);
 
-	function calculatePrice(columns: number, rows: number) {
-		const factor = Math.pow(10, 2);
-		const base_price = (Math.pow(2, columns * 0.1) * 0.1177) * ((columns) + (rows * 0.5));
-		const rounded_price = Math.round(base_price * factor) / factor;
-		return rounded_price;
+	function calculatePrice(columns: number, rows: number, pollFrequency: number) {
+		if (pollFrequency === 0 && columns <= 15 && rows <= 150) {
+			return "Free";
+		}
+		else if (pollFrequency === 3 || columns > 25 || rows > 250) {
+			return "Enterprise";
+		} else {
+			const factor = Math.pow(10, 2);
+			let price = (Math.pow(2, columns * 0.1) * 0.1177) * ((columns) + (rows * 0.5));
+			if (pollFrequency === 2) {
+				price += 20;
+			}
+			if (pollFrequency === 0) {
+				price /= 6.34;
+			}
+			return "$" + Math.round(price * factor) / factor;
+		}
+	}
+
+	function translatePollFrequency(pollFrequency: number) {
+		switch (pollFrequency) {
+			case 2:
+				return "Daily";
+			case 1:
+				return "Weekly";
+			case 0:
+				return "Monthly";
+		}
 	}
 
 	function handlePurchase() {
@@ -22,6 +46,7 @@ const Pricing = (props: {}) => {
 	function reset() {
 		setColumns([5]);
 		setRows([50]);
+		setPollFrequency([1]);
 	}
 
 
@@ -41,9 +66,13 @@ const Pricing = (props: {}) => {
 							<label>Number of rows you'd like analyzed</label>
 							<Slider className="my-4" value={rows} onValueChange={setRows} defaultValue={rows} max={260} step={10} />
 							{(rows[0] === 260) ? <p>Enterprise</p> : <p>{rows[0]}</p>}
+							<br></br>
+							<label>Frequency of polling</label>
+							<Slider className="my-4" value={pollFrequency} onValueChange={setPollFrequency} defaultValue={pollFrequency} max={3} step={1} />
+							{(pollFrequency[0] === 3) ? <p>Enterprise</p> : <p>{translatePollFrequency(pollFrequency[0])}</p>}
 							<div className="flex mt-7">
 								<strong>Your price:&nbsp;</strong>
-								{(columns[0] === 26 || rows[0] === 260) ? <p>Enterprise</p> : <p>${PRICE}/month</p>}
+								{calculatePrice(columns[0], rows[0], pollFrequency[0])}
 							</div>
 							<div className="mt-7 mx-12 flex justify-around items-center">
 								<Button variant="outline" className="w-32 hover:bg-gray-200/10" onClick={reset}>Reset</Button>
