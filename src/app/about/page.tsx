73aab1d60f64beea31e7d34abf6940347009d81a
Jake Spievak <jakespvk@gmail.com>
2025-03-11 19:16:11 -0700

    styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
                 </p>
                 <h2 className="text-gray-200 text-3xl pb-5 text-center">Demo</h2>
                 <div className="flex justify-center">
-                    <iframe className="pb-5 w-[250px] h-[140px] min-[415px]:w-[375px] min-[415px]:h-[211px] sm:w-[560px] sm:h-[315px]" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe className="pb-5 max-w-4/5 w-[250px] h-[140px] min-[415px]:w-[375px] min-[415px]:h-[211px] sm:w-[560px] sm:h-[315px] xl:w-[1050px] xl:h-[590px] min-[1800px]:w-[1600px] min-[1800px]:h-[900px]" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </div>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
