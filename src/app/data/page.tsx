e2acf5ed3faf572da7d57fbcba82fa774b3582b3
Jake Spievak <jakespvk@gmail.com>
2025-04-12 17:19:07 -0700

    scaffold yourdata page

---
+++ src/app/data/page.tsx
@@ -0,0 +1,15 @@
+export default function YourData() {
+    return (
+        <>
+            <div className="flex items-center justify-center [@media(min-width:450px)]:h-screen">
+                <div className="[@media(min-width:450px)]:glass-card p-6">
+                    <h1 className="text-3xl text-center">Your Data</h1>
+                    <p className="mt-6">
+                        Your data is secure. The reason we use Magic Email Link login
+                        is to ensure your data&apos;s security.
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
